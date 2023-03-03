7a13730236b98795438c1cad7058832a63f0079c
Greg Tambornino <greg.tambornino@concordusa.com>
2023-03-03 11:45:57 -0600

    migrate demo code to use swiper v9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SlidesComponent } from './slides/slides.component';
 import { register } from 'swiper/element/bundle';
+import { SwiperDirective } from './swiper.directive';
+import { CommonModule } from '@angular/common';
 // register Swiper custom elements
 register();
 
@@ -16,7 +18,9 @@ register();
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule    
+    AppRoutingModule, 
+    SwiperDirective,
+    CommonModule
   ],
   schemas: [
     CUSTOM_ELEMENTS_SCHEMA],
--- src/app/slides/slides.component.ts
+++ src/app/slides/slides.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+
+// slides.component.ts
+
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import { BehaviorSubject } from 'rxjs';
+import {A11y, Mousewheel, Navigation, Pagination, SwiperOptions} from 'swiper';
+import { SwiperDirective } from '../swiper.directive';
+
+// import SwiperCore, { Keyboard, Pagination, Navigation, Virtual } from 'swiper';
+
+// SwiperCore.use([Keyboard, Pagination, Navigation, Virtual]);
 
 @Component({
   selector: 'corp-slides',
   templateUrl: './slides.component.html',
-  styleUrls: ['./slides.component.scss']
+  styleUrls: ['./slides.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class SlidesComponent {
+export class SlidesComponent implements OnInit {
 
+  slides$ = new BehaviorSubject<string[]>(['']);
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.slides$.next(
+      Array.from({ length: 600 }).map((el, index) => `Slide ${index + 1}`)
+    );
+  }
+
+  public config: SwiperOptions = {
+    modules: [Navigation, Pagination, A11y, Mousewheel],
+    autoHeight: true,
+    spaceBetween: 100,
+    navigation: true,
+    pagination: {clickable: true, dynamicBullets: true},
+    slidesPerView: 1,
+    centeredSlides: false,
+    breakpoints: {
+      400: {
+        slidesPerView: "auto",
+        centeredSlides: false
+      },
+    }
+  }
 }
